6eb61cd4a85fdfceb3ffacecb6b7a2e0ace3f2d2
Klaudiusz <florekklaudiusz@gmail.com>
2022-05-29 16:45:15 +0200

    test pass

--- __tests__/shoppingCart.spec.ts
+++ __tests__/shoppingCart.spec.ts
@@ -1,25 +1,18 @@
-import cartTotal from "../src/shoppingCart";
+import { handleAddProduct, handleCartTotal } from "../src/shoppingCart";
+import { handleProducts } from "../src/shoppingCart";
 
-const products = [
-  {
-    name: "ball",
-    price: 10,
-  },
-  {
-    name: "boots",
-    price: 125,
-  },
-  {
-    name: "gloves",
-    price: 35,
-  },
-];
+const products = handleProducts();
 
-const addedProduct = {};
-const newProducts: Array<{ name: string; price: number }> = [];
+const addedProduct = {
+  id: 3,
+  name: "smartphone",
+  price: 400,
+};
+const newProducts: Array<{ id: number; name: string; price: number }> =
+  handleAddProduct(products, addedProduct);
 
-const total = cartTotal([]);
-const total2 = cartTotal(products);
+const total = handleCartTotal([]);
+const total2 = handleCartTotal(products);
 
 describe("cartTotal", () => {
   it("should return 0 if cart is empty", () => {
@@ -27,7 +20,7 @@ describe("cartTotal", () => {
   });
   it("should return sum of all product values in cart", () => {
     expect(total2).toBeGreaterThan(0);
-    expect(total2).toEqual(170);
+    expect(total2).toEqual(570);
   });
 });
 
@@ -35,14 +28,17 @@ describe("addToCart", () => {
   it("should add product to cart", () => {
     expect(newProducts).toEqual([
       {
+        id: 0,
         name: "ball",
         price: 10,
       },
       {
+        id: 1,
         name: "boots",
         price: 125,
       },
       {
+        id: 2,
         name: "gloves",
         price: 35,
       },
--- src/shoppingCart.ts
+++ src/shoppingCart.ts
@@ -1,9 +1,43 @@
-const cartTotal = (
+export function handleProducts() {
+  return [
+    {
+      id: 0,
+      name: "ball",
+      price: 10,
+    },
+    {
+      id: 1,
+      name: "boots",
+      price: 125,
+    },
+    {
+      id: 2,
+      name: "gloves",
+      price: 35,
+    },
+  ];
+}
+
+export function handleAddProduct(
+  products: Array<{
+    id: number;
+    name: string;
+    price: number;
+  }>,
+  newProduct: { id: number; name: string; price: number }
+) {
+  products.push(newProduct);
+  console.log(products);
+  return products;
+}
+
+export function handleCartTotal(
   cart: Array<{
+    id: number;
     name: string;
     price: number;
   }>
-) => {
+) {
   let total: number;
   if (cart.length === 0) {
     total = 0;
@@ -12,6 +46,4 @@ const cartTotal = (
     total = cart.reduce((sum, { price }: { price: number }) => sum + price, 0);
     return total;
   }
-};
-
-export default cartTotal;
+}
