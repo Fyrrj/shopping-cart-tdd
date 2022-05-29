8a041fafafb9a998f557880a7559dbe1134399e0
Klaudiusz <florekklaudiusz@gmail.com>
2022-05-29 14:18:06 +0200

    pass for second expect

--- __tests__/cartTotal.spec.ts
+++ __tests__/cartTotal.spec.ts
@@ -1,12 +1,18 @@
 import cartTotal from "../src/shoppingCart";
 
 const total = cartTotal([]);
+const total2 = cartTotal([
+  {
+    name: "ball",
+    price: 10,
+  },
+]);
 
 describe("cartTotal", () => {
   it("should return 0 if cart is empty", () => {
     expect(total).toEqual(0);
   });
   it("should return sum of all product values in cart", () => {
-    expect(total).toBeGreaterThan(0);
+    expect(total2).toBeGreaterThan(0);
   });
 });
--- src/shoppingCart.ts
+++ src/shoppingCart.ts
@@ -8,6 +8,9 @@ const cartTotal = (
   if (cart.length === 0) {
     total = 0;
     return total;
+  } else {
+    total = cart.reduce((sum, { price }: { price: number }) => sum + price, 0);
+    return total;
   }
 };
 
